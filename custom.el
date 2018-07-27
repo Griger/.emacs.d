46f4df5df2eeaab72fbb5c8cf309e10280d2cf80
griger <gustavo.rg92@gmail.com>
2018-07-27 12:37:05 +0200

    Updating

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex")
  '(TeX-view-program-list (quote (("Okular" ("okular %o") nil))))
  '(TeX-view-program-selection
    (quote
